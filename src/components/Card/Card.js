c2eb7a0aac0f6510bf6231bbf60b67ff1fe3a6f1
mauricio sandoval <flashpoint_84@hotmail.com>
2022-03-23 19:14:49 -0300

    Se genera el botón Agregar

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,27 +1,18 @@
-import React, { useState } from "react";
-import Button from '@mui/material/Button';
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
 import './_card.scss';
 import ItemCartCount from '../ItemCartCount/ItemCartCount';
 
 export default function Card ({title, pic ,price}) {
-
+    
     return (
         <div className='card'>
             <h2>{title}</h2>
             <img src={pic} alt='Consola'/>
             <p>Precio: ${price}</p>
+            <ItemCartCount
+                stock={5}
+                initial={1}
+            />
 
-            <ItemCartCount />
-
-            <Button
-                variant='contained'
-                color="secondary">
-                Agregar
-                
-                <AddShoppingCartOutlinedIcon fontSize="small"/>
-
-            </Button>
         </div>
     )
 }
--- src/components/ItemCartCount/ItemCartCount.js
+++ src/components/ItemCartCount/ItemCartCount.js
@@ -2,28 +2,59 @@ import React, { useState } from "react";
 import Button from '@mui/material/Button';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
 
-function ItemCartCount (){
+function ItemCartCount ({stock, initial}){
 
-    const [stock, setStock] = useState(30);
-    const [initial, setInitial] = useState(0);
+  const [count, setCount] = useState(initial);
+  const [disableUp, setDisableUp] = useState(false);
+  const [disableDown, setDisableDown] = useState(false);
+  const onAdd = ()=>{
+    console.log(count);
+  }
 
-    const onAdd = () =>{
-        setInitial(initial+1);
+
+  const setItemCountUp = ()=>{
+    setCount(count+1);
+    if(count === stock-1){
+      console.log('Stock alcanzado');
+      setDisableUp(true);
+    }else{
+      setDisableDown(false);
     }
+  }
 
-    const onRemove = () =>{
-        setInitial(initial-1);
+  const setItemCountDown = () =>{
+    setCount(count-1);
+    if(count <= initial+1){
+      console.log('Cantidad menor a inicial');
+      setDisableDown(true);
+      
+    }else{
+      setDisableUp(false);
     }
+  }
 
   return (
     <div className="item-cart-display">
-      <Button>
-            <RemoveOutlinedIcon onClick={onRemove}/>
+      <Button
+        onClick={setItemCountDown}
+        disabled={disableDown}
+        >
+            <RemoveOutlinedIcon/>
       </Button>
-        <span>{initial}</span>
-      <Button>
-            <AddOutlinedIcon onClick={onAdd}/>
+        <span>{count}</span>
+      <Button
+        onClick={setItemCountUp}
+        disabled={disableUp}>
+            <AddOutlinedIcon/>
+      </Button>
+      <Button
+          onClick={onAdd}
+          variant='contained'
+          color="secondary">
+          Agregar     
+          <AddShoppingCartOutlinedIcon fontSize="small"/>
       </Button>
     </div>
   );
